eb512e9b05b006495da5eba48c1c6260f0ff0298
laibakhab <asifalikhan4485@gmail.com>
2025-01-21 17:03:56 -0800

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +20,10 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-        {/* <Header/> */}
-        {children}</body>
-      {/* <Footer/> */}
+       
+        {children} <Header/>
+        </body>
+      <Footer/>
     </html>
   );
 }
